8a8cfb2c8bb9594d2d06995dad7ff044f82827a0
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-28 12:56:27 +0200

    Create EntryPoint

---
+++ RayTracing/src/RayTracing/EntryPoint.cpp
@@ -0,0 +1,26 @@
+#include <Walnut/Application.h>
+#include <Walnut/EntryPoint.h>
+
+#include "RayTracing/Layer/MainLayer/MainLayer.h"
+
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "Ray Traceing";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<RayTracing::MainLayer>();
+	app->SetMenubarCallback([app]()
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Exit"))
+				{
+					app->Close();
+				}
+				ImGui::EndMenu();
+			}
+		});
+	return app;
+}
