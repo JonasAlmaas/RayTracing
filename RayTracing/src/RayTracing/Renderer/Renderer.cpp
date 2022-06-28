51718fa47244e504a10b99779fd09f0a5ca9977a
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-28 13:13:54 +0200

    Very basic ray tracing

--- RayTracing/src/RayTracing/Renderer/Renderer.cpp
+++ RayTracing/src/RayTracing/Renderer/Renderer.cpp
@@ -31,6 +31,7 @@ namespace RayTracing {
 			for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 			{
 				glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
+				coord = coord * 2.0f - 1.0f; // Remap from 0 -> 1 to -1 to 1;
 				m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 			}
 		}
@@ -42,7 +43,32 @@ namespace RayTracing {
 	{
 		uint8_t r = (uint8_t)(coord.x * 255.0f);
 		uint8_t g = (uint8_t)(coord.y * 255.0f);
-		return 0xff000000 | g << 8 | r ;
+
+		glm::vec3 rayOrigin{ 0.0f, 0.0f, 2.0f };
+		glm::vec3 rayDirection{ coord.x, coord.y, -1.0f };
+		//rayDirection = glm::normalize(rayDirection);
+		float radius = 0.5f;
+
+		// (b.x^2 + b.y^2) * t^2 + (2(a.x * b.x + a.y * b.y)) * t + (a.x^2 + a.y^2 - r^2) = 0
+
+		// a = ray origin
+		// b = ray direction
+		// r = radius on sphere
+		// t = hit distance
+
+		float a = glm::dot(rayDirection, rayDirection);
+		float b = 2 * glm::dot(rayOrigin, rayDirection);
+		float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+		// Quadratic formula discriminant:
+		// b^2 - 4ac
+
+		float discriminant = b * b - 4.0f * a * c;
+
+		if (discriminant >= 0)
+			return 0xffff00ff;
+
+		return 0xff000000;
 	}
 
 }
